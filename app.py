715b3603c0c9be66ddbdd8a8d6be62e47b9a5f93
Bartlomiej Bialek <bialcus@gmail.com>
2018-05-09 20:37:01 +0100

    Added basic user commands.

--- app.py
+++ app.py
@@ -4,11 +4,26 @@ def main():
 
 
 def print_header():
-    pass
+    print('--------------------')
+    print('      JOURNAL-CLI')
+    print('--------------------')
 
 
 def run_event_loop():
-    pass
+    print('Choose one of the options: ')
+    user_cmd = None
+
+    while user_cmd != 'q':
+        user_cmd = input()
+
+        if user_cmd == 'l':
+            print('')
+        elif user_cmd == 'a':
+            print('')
+        elif user_cmd != 'q':
+            print("Sorry, unknown command '{}'.".format(user_cmd))
 
 
-main()
+
+if __name__ == '__main__':
+    main()
