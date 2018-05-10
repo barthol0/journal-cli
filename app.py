34c70ae631139614e5469ddacfbe51a592f4f86f
Bartlomiej Bialek <bialcus@gmail.com>
2018-05-10 21:18:45 +0100

    Refactor.

--- app.py
+++ app.py
@@ -10,18 +10,23 @@ def print_header():
 
 
 def run_event_loop():
-    print('Choose one of the options: ')
-    user_cmd = None
+    print('Choose one of the options:\n')
+    print('[L]ist entries.')
+    print('[A]dd entry.')
+    print('[Q]uit.')
+    user_cmd = ''
 
     while user_cmd != 'q':
         user_cmd = input()
+        user_cmd = user_cmd.lower().strip()
 
         if user_cmd == 'l':
-            print('')
-        elif user_cmd == 'a':
-            print('')
+            print('list entries')
+        elif  user_cmd == 'a':
+            print('add entry')
         elif user_cmd != 'q':
-            print("Sorry, unknown command '{}'.".format(user_cmd))
+            print("Sorry, unknown command '{}'.".format(user_cmd.lower().strip()))
+            
 
 
 
